53b2cbf0495658e5900f481d6005b71d067e8fa2
rubicks <rubicks@users.noreply.github.com>
2014-01-17 16:28:33 -0500

    small additions to the home page

--- src/choreo/routes/home.clj
+++ src/choreo/routes/home.clj
@@ -6,16 +6,32 @@
    [choreo.views.layout :as layout]
    ))
 
-(defn home-page [& [name message error]]
+
+(defn- -home-page [& [name message error]]
   (layout/render "home.html"
                  {:error error
                   :name name
                   :message message
                   :messages (db/get-messages)}))
 
-(defn about-page []
+
+(defn- -save-message [name message]
+  (cond
+   (empty? name)
+   (-home-page name message "Someone forgot to leave a name.")
+   (empty? message)
+   (-home-page name message "Don't you have something to say?")
+   :else
+   (do
+     (db/save-message name message)
+     (-home-page))))
+
+
+(defn- -about-page []
   (layout/render "about.html"))
 
+
 (defroutes home-routes
-  (GET "/" [] (home-page))
-  (GET "/about" [] (about-page)))
+  (GET "/" [] (-home-page))
+  (GET "/" [name message] (-home-page name message))
+  (GET "/about" [] (-about-page)))
